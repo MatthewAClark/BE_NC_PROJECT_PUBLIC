ef787821e18df6bc4f5bd4888d7b503ce82e6b92
MatthewAClark <matthewclark86@gmail.com>
2018-09-15 15:53:44 +0100

    trying to resolve problems

--- routes/db.stations.js
+++ routes/db.stations.js
@@ -2,37 +2,7 @@ const express  = require('express');
 const router = express.Router();
 const { removeStation, addNewStation, fetchStation, fetchStationById} = require('../controllers/db.stations');
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
 router.get('/', fetchStation);
-
-router.get('/test', async (req, res) => {
-    console.log('getting data test')
-       // try {
-        //  const client = await pool.connect()
-          pool.connect().then(db => {
-            db.query('SELECT * FROM train_stations')
-            .then(result2 => {
-                db.release()
-                console.log('this is result 2',result2.rows)
-            }) 
-            ;
-
-          })
-         // const result = await client.query('SELECT * FROM train_stations');
-         // const results = { 'results': (result) ? result.rows : null};
-          //console.log(results)
-         // res.render('pages/db', results );
-         // client.release();
-      //  } catch (err) {
-         // console.error(err);
-         // res.send("Error " + err);
-      //  }
-      })
 
 
 router.get('/:station_id', fetchStationById);
