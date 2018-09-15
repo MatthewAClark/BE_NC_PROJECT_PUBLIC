e208a65cf1371c928afe20e6e1813f8809a19144
MatthewAClark <matthewclark86@gmail.com>
2018-09-15 13:16:58 +0100

    trying to resolve problems

--- config/production.js
+++ config/production.js
@@ -6,17 +6,17 @@ const pool = new Pool({
 });
 
 //.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
+   // try {
+      const client = pool.connect()
+      const result = client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
      // res.render('pages/db', results );
      console.log(results)
       client.release();
-    } catch (err) {
+ //   } catch (err) {
       console.error(err);
       res.send("Error " + err);
-    }
+  //  }
 //  })
 
 db = pool.connect()
